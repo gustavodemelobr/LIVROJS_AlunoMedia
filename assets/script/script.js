f9fbac3e34de024a5e24c4ec556b2c38312df4cd
Gustavo de Melo <gustavo.gna@hotmail.com>
2023-10-12 20:45:01 -0300

    Exercicio para calcular media escolar do aluno

---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+const frm = document.querySelector("form");
+const media = document.querySelector(".outMedia");
+const msg = document.querySelector(".outMsg");
+
+frm.addEventListener("submit", (e) => {
+    
+})
